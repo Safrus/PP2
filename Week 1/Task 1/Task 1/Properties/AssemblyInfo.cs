fd26682169fb1d10589a05b59f1f658dad9dedf6
Safrus <rus.safarov222@gmail.com>
2019-01-26 13:29:26 +0600

    fd

--- Week 1/Task 1/Task 1/Properties/AssemblyInfo.cs
+++ Week 1/Task 1/Task 1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Task 1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Task 1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("d7c65a24-2c9f-4c51-9f1f-3193a2ba638f")]
+[assembly: Guid("429d3e98-948c-40f7-9943-663fcf04d832")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
