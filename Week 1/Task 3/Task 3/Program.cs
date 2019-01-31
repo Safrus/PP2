dcad52e7056de5567486cd32575e73e6343eda72
Safrus <rus.safarov222@gmail.com>
2019-02-01 00:38:20 +0600

    yeah

---
+++ Week 1/Task 3/Task 3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        
+        
+            static void prikol(int[] b) // making new function
+            {
+                List<int> glist = new List<int>();// create the list
+                foreach (int k in b)
+                {
+                    glist.Add(k);// making foreach cycle to add each k twice at array b to list "glist"
+                    glist.Add(k);
+                }
+                foreach (int k in glist)
+                { Console.Write(k + " "); }// show the final result 
+            }
+
+            static void Main(string[] args)
+            {
+
+                int n = int.Parse(Console.ReadLine());// create int n and convert string to int
+
+                string s = Console.ReadLine();// create string s and enter it
+
+                string[] a = s.Split();// divide string s to substrings without " "(space)
+                int[] b = new int[n];// create a new array of int
+
+                for (int i = 0; i < n; i++) // making cycle
+                {
+                    b[i] = int.Parse(a[i]);// enter substrings to an array b as int 
+                }
+
+                prikol(b); // trigger the function
+
+                Console.ReadKey();// obtains the next character or function key pressed by the user
+
+
+
+            }
+        }
+    }
+
+    
+
--- Week 1/Task 3/Task 3/Properties/AssemblyInfo.cs
+++ Week 1/Task 3/Task 3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task 1")]
+[assembly: AssemblyTitle("Task 3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task 1")]
+[assembly: AssemblyProduct("Task 3")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("429d3e98-948c-40f7-9943-663fcf04d832")]
+[assembly: Guid("12160fa0-15af-41ec-82c0-e2a30fe4185b")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
