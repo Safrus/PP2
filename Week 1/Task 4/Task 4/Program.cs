8246ea5f9b8e11d07fb9d8220ea3ed2e63326dbe
Safrus <rus.safarov222@gmail.com>
2019-02-01 00:53:23 +0600

    new

--- Week 1/Task 4/Task 4/Program.cs
+++ Week 1/Task 4/Task 4/Program.cs
@@ -10,6 +10,16 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());// create int n and convert string to int
+
+            for (int i = 1; i <= n; i++) // making cycle and show [*] for each row and column
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("[*]");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();// obtains the next character or function key pressed by the user
         }
     }
 }
